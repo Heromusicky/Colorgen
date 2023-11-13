3a90007e8955e9893fb4570d2cf71f93f7493a50
heromusicky <Jhorlaitan1@gmail.com>
2023-11-13 16:02:49 +0100

    color generator

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Colorlist from './Colorlist';
+import Form from './Form';
+import Values from 'values.js';
+import { ToastContainer, toast } from 'react-toastify';
+
+const App = () => {
+  const[colors,setColors]=useState(new Values('#f15025').all(10))
+ const addColor=(color)=>{
+  try {
+    const newColors =new Values(color).all(10)
+    setColors(newColors)
+    
+  } catch (error) {
+    console.log(error.message);
+    toast.error(error.message)
+    
+  }
+
+ }
+
+  return (
+    <main>
+     
+      <Form  addColor={addColor}/>
+      <Colorlist colors={colors}/>
+      <ToastContainer/>
+    </main>
+  );
+};
+export default App;
